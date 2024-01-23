a224a5a2725bd16a4735983add0889e7dc9c01db
Nuraliev Alirahmon <119412119+Alirahmon777@users.noreply.github.com>
2024-01-23 15:53:43 +0500

    profile-page-full

--- src/components/ui/HistoryTable.tsx
+++ src/components/ui/HistoryTable.tsx
@@ -1,35 +1,42 @@
+import { cn } from '@/lib/utils';
 import decline from '@svgs/layout/decline.svg';
 import success from '@svgs/layout/successfull.svg';
-const HistoryTable = () => {
+import { HTMLAttributes } from 'react';
+
+interface IProps extends HTMLAttributes<HTMLTableElement> {
+  requisites?: string;
+}
+
+const HistoryTable = ({ className, requisites, ...props }: IProps) => {
   return (
-    <table className='w-full border-separate border-spacing-y-5'>
+    <table className={cn('w-full border-separate border-spacing-y-5', className)} {...props}>
       <thead className='text-gray'>
         <tr className='[&_th]:!font-normal [&_th]:bg-header [&_th]:max-w-[101px]'>
           <th className='text-left pl-6 py-3 rounded-l-[10px]'>Номер заказа</th>
           <th className='text-left'>Дата/Время</th>
           <th className='text-left'>Метод оплаты</th>
-          <th className=''>Реквизиты</th>
+          <th className={requisites}>Реквизиты</th>
           <th className='text-left'>Сумма ¥ - ₽</th>
           <th className='rounded-r-[10px] pr-6 text-left'>Статус</th>
         </tr>
       </thead>
-      <tbody>
-        <tr className='[&_td]:bg-header'>
+      <tbody className='[&_td]:bg-[#1D1F1E]'>
+        <tr>
           <td className='py-3 pl-6 rounded-l-[10px]'>14</td>
           <td className=''>2024.01.17/00:00</td>
           <td className=''>Tinkoff</td>
-          <td className=' text-center'>2200 7009 3558 9290</td>
+          <td className={requisites ? requisites : 'text-center'}>2200 7009 3558 9290</td>
           <td className=''>1000¥ - 13000₽</td>
           <td className='rounded-r-[10px] flex gap-1 min-h-full py-3'>
             <img src={decline} alt='' />
             Отклонен
           </td>
         </tr>
-        <tr className='[&_td]:bg-header'>
+        <tr>
           <td className='py-3 pl-6 rounded-l-[10px]'>13</td>
           <td className=''>2024.01.17/00:00</td>
           <td className=''>Tinkoff</td>
-          <td className=' text-center'>2200 7009 3558 9290</td>
+          <td className={requisites ? requisites : 'text-center'}>2200 7009 3558 9290</td>
           <td className=''>1000¥ - 13000₽</td>
           <td className='rounded-r-[10px] flex gap-1 min-h-full py-3'>
             <img src={success} alt='' />
--- src/pages/profile/page.tsx
+++ src/pages/profile/page.tsx
@@ -2,6 +2,7 @@ import ProfileData from '@/components/profile/ProfileData';
 import ProfileInfo from '@/components/profile/ProfileInfo';
 import ProfileLevel from '@/components/profile/ProfileLevel';
 import ProfileTrade from '@/components/profile/ProfileTrade';
+import HistoryTable from '@/components/ui/HistoryTable';
 
 const ProfilePage = () => {
   return (
@@ -12,6 +13,23 @@ const ProfilePage = () => {
           <ProfileTrade />
           <ProfileInfo />
           <ProfileLevel />
+        </div>
+        <div className='mt-[60px]'>
+          <div className='flex items-center justify-between'>
+            <h4 className='text-2xl font-medium'>История продаж</h4>
+            <div className='text-gray font-medium [&_span]:text-white flex items-center gap-10'>
+              <p>
+                Всего сделок: <span>0</span>
+              </p>
+              <p>
+                Продано на: <span>0.00¥</span>
+              </p>
+            </div>
+          </div>
+          <HistoryTable
+            className='[&_th]:py-[20px] [&_tbody]:before:content-[""] [&_tbody]:before:table-row'
+            requisites='text-left'
+          />
         </div>
       </div>
     </section>
