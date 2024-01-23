f949f34f0daaf3b125ebb8f084238c78d6e7efc4
Nuraliev Alirahmon <119412119+Alirahmon777@users.noreply.github.com>
2024-01-23 14:30:21 +0500

    fixed

--- src/components/layout/header/header-data.ts
+++ src/components/layout/header/header-data.ts
@@ -1,15 +1,15 @@
 import RUB from '@svgs/currencies/rub.svg';
 import KZT from '@svgs/currencies/kzt.svg';
 import USD from '@svgs/currencies/usd.svg';
-import { ILanguageCurrency } from './types/interface';
+import { ILanguageCurrencyNav } from './types/interface';
 
-export const currencies: ILanguageCurrency[] = [
+export const currencies: ILanguageCurrencyNav[] = [
   { label: 'rub', icon: RUB },
   { label: 'usd', icon: USD },
   { label: 'kzt', icon: KZT },
 ];
 
-export const languages: ILanguageCurrency[] = [
+export const languages: ILanguageCurrencyNav[] = [
   { label: 'rus', lang: 'ru' },
   { label: 'eng', lang: 'en' },
 ];
