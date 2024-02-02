5c38050559250b5479913e54a90bfafc3936b153
Nuraliev Alirahmon <119412119+Alirahmon777@users.noreply.github.com>
2024-02-02 17:34:10 +0500

    contact-responsive

--- src/components/help/HelpButtons.tsx
+++ src/components/help/HelpButtons.tsx
@@ -9,11 +9,15 @@ interface IProps {
 const HelpButtons = ({ label }: IProps) => {
   return (
     <>
-      <Button label={label} leftIcon={commentAltIcon} className='py-2 lg:py-[14px] px-6 rounded-[10px] gap-2' />
+      <Button
+        label={label}
+        leftIcon={commentAltIcon}
+        className='py-2 lg:py-[14px] justify-center px-6 rounded-[10px] gap-2'
+      />
       <Button
         label='Телеграм'
         leftIcon={telegramIcon}
-        className='py-2 lg:py-[14px] px-6 bg-header [&_p]:text-white rounded-[10px] gap-2'
+        className='py-2 lg:py-[14px] px-6 bg-header [&_p]:text-white justify-center rounded-[10px] gap-2'
       />
     </>
   );
